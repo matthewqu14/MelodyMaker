57c34a38140b3c6de2ba7a4bad45258b6b6219c9
matthewqu14 <matthewqu@college.harvard.edu>
2019-12-04 02:00:30 -0500

    email verification added.

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sqlite3
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -12,6 +14,13 @@ from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
+app.config.from_pyfile("config.cfg")
+
+mail = Mail(app)
+
+s = URLSafeTimedSerializer("Thisisasecret!")
+
+tempid = ""
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -38,9 +47,11 @@ db.execute("""CREATE TABLE IF NOT EXISTS users (
             hash TEXT NOT NULL,
             confirmation BOOLEAN DEFAULT false);""")
 
+db.execute("DELETE FROM users WHERE username = ?", "muffinmatt8@gmail.com")
 @app.before_request
 def make_session_permanent():
     session.permanent = True
+
 
 @app.route("/")
 def index():
@@ -63,12 +74,12 @@ def login():
         # Ensure username was submitted
         if not request.form.get("username"):
             flash("Please enter a username.")
-            return redirect("/login")
+            return render_template("login.html")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
             flash("Please enter a password.")
-            return redirect("/login")
+            return render_template("login.html")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
@@ -76,9 +87,12 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            flash("Invalid username and/or password")
-            return redirect("/login")
+            flash("Invalid username and/or password.")
+            return render_template("login.html")
 
+        if not rows[0]["confirmation"]:
+            flash("You have not yet confirmed your email address.")
+            return render_template("login.html")
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
@@ -88,6 +102,7 @@ def login():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
+        session.clear()
         return render_template("login.html")
 
 
@@ -135,18 +150,38 @@ def register():
         if username_exist[0]["COUNT(*)"] != 0:
             flash("Username already taken.")
             return redirect("/register")
+        token = s.dumps(username, salt="confirm")
+        msg = Message("Confirm Your MelodyMaker Account", sender='melodymakerpro@gmail.com', recipients=[username])
+        link = url_for("confirm", token=token, _external=True)
+        msg.body = "Verify your MelodyMaker Account by clicking on the link below. " \
+                   "Please be aware that this link will expire in 20 minutes; " \
+                   "afterwards, you will have to register your account again.\n\n{}".format(link)
+        try:
+            mail.send(msg)
+        except:
+            flash("Sorry! That doesn't look like an email.")
+            return redirect("/register")
         db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, generate_password_hash(password))
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                           username=request.form.get("username"))
-        session["user_id"] = rows[0]["id"]
-        flash("TODO")
-        return redirect("/")
+        global tempid
+        tempid = rows[0]["id"]
+        return render_template("confirmation.html", token=token, email=username)
 
 
-@app.route("/confirmation")
-@login_required
-def confirmation():
-    return render_template
+@app.route('/confirm/<token>')
+def confirm(token):
+    global tempid
+    try:
+        s.loads(token, salt="confirm", max_age=1200)
+    except SignatureExpired:
+        db.execute("DELETE FROM users WHERE id = ?", tempid)
+        flash("Your link has expired. Please register your account again.")
+        session["user_id"] = None
+        return redirect("/register")
+    db.execute("UPDATE users SET confirmation = true WHERE id = ?", tempid)
+    session["user_id"] = tempid
+    return render_template("welcome.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
